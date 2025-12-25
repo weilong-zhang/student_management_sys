6dc6349fecdf5acebaac366907056093844e3a7b
itzhang <2019326567@qq.com>
2025-12-25 11:52:25 +0800

    优化了部分代码

---
+++ src/test/java/com/itzhang/management/StudentManagementSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itzhang.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
